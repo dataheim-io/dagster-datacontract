22e96e74df70bfa097ac02c928cdc437eceb36c4
Fredrik Bakken <fredrik@dataheim.io>
2025-04-11 19:59:33 +0200

    fix(cd): release pipeline (#8)
    Signed-off-by: Fredrik Bakken <fredrik@dataheim.io>

--- .github/validate-release-version.py
+++ .github/validate-release-version.py
@@ -1,15 +1,14 @@
-"""Ensures that the files in `dist/` are prefixed with ${{ github.ref_name }}
+"""Ensures that the files in `dist/` are prefixed with {build_name}
 
 Tag must adhere to naming convention of distributed files. For example, the tag
-`dagster_datacontract-0.1.2` must match the prefix of the files in the `dist/` folder:
+`v0.1.2` must match the prefix of the files in the `dist/` folder:
 
     -rw-r--r--@ 2.0K Oct 23 14:06 dagster_datacontract-0.1.2-py3-none-any.whl
     -rw-r--r--@ 1.6K Oct 23 14:06 dagster_datacontract-0.1.2.tar.gz
 
 USAGE
 
-    $ python .github/validate-release-version.py libraries/dagster_datacontract/dist dagster_datacontract-0.1.3
-
+    $ python .github/validate-release-version.py ./dist v0.2.0
 """
 
 import sys
@@ -21,7 +20,8 @@ if len(sys.argv) != 3:
     sys.exit(1)
 
 dist_path = sys.argv[1]
-github_ref_name = sys.argv[2]
+version = sys.argv[2]
+build_name = f"dagster_datacontract-{version.replace('v', '')}"
 
 if not os.path.exists(dist_path):
     print("Release directory `dist/` must exist")
@@ -30,9 +30,9 @@ if not os.path.exists(dist_path):
 for filename in os.listdir(dist_path):
     if filename.startswith("."):
         continue
-    if not filename.startswith(github_ref_name):
-        print(f"{filename} does not start with prefix {github_ref_name}")
+    if not filename.startswith(build_name):
+        print(f"{filename} does not start with prefix {build_name}")
         sys.exit(1)
 
 
-print(f"Success: all files in `dist/` are prefixed with {github_ref_name}")
+print(f"Success: all files in `dist/` are prefixed with {build_name}")
